696497781480c3d71be459fd1de6e85df90a6ff6
Cola <99972634+Cola-hk@users.noreply.github.com>
2022-04-04 22:57:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const btn = document.getElementById("button");
 const colour = document.getElementById("hex");
 
-console.log(btn)
-
 btn.addEventListener("click",() => {
     randomColour = Math.floor(Math.random()*16777215).toString(16);
     console.log(randomColour)
